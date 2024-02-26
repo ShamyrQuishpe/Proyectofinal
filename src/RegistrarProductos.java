0ee5b1afb1f93a48db755f2445618858b1257ea0
Monica Jaña <monicajana1804@gmail.com>
2024-02-26 15:27:33 -0500

    mostrar productos

--- src/RegistrarProductos.java
+++ src/RegistrarProductos.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -6,8 +8,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Base64;
 
 public class RegistrarProductos extends JFrame {
     private JTextField nombreP;
@@ -17,15 +19,39 @@ public class RegistrarProductos extends JFrame {
     private JButton GUARDARButton;
     private JPanel panel1;
     private JTextArea descripcionP;
-    private JLabel rutaimagen;
+    private JScrollPane scrollPane;
+    private JTable tableR;
+    private JPanel panelDatos;
+    private JPanel panelTabla;
+    private JTextField textField1;
+    private JButton buscarButton;
+    private JButton actualizarButton;
+    private JButton eliminarButton;
+
+    private static byte[] imagen;
 
     public RegistrarProductos(){
         super("Registrar Productos");
         setContentPane(panel1);
+
+        //hacer que el texto de la descripcion se adapte al tamanio de la pantalla
+        descripcionP.setLineWrap(true);
+        descripcionP.setWrapStyleWord(true);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        //Dar tamanio al panel de los datos y al panel de la tabla
+        // Establecer un tamaño específico para panelSecundario
+        panelDatos.setPreferredSize(new Dimension(300, 300));
+        panelTabla.setPreferredSize(new Dimension(500, 400));
+
+        //Cargar los repuestos ingresados
+        mostrarProductos_tabla();
+
         subirImagenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                subirImagen(false);
+
+               imagen= subirImagen(false);
             }
         });
         GUARDARButton.addActionListener(new ActionListener() {
@@ -38,7 +64,7 @@ public class RegistrarProductos extends JFrame {
 
     public void iniciar(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400,500);
+        setSize(900,600);
         setLocationRelativeTo(null);
         setVisible(true);
     }
@@ -51,7 +77,7 @@ public class RegistrarProductos extends JFrame {
         try {
              cantidad =Integer.parseInt(cantidadP.getText());
         }catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(null,"Solo puede ingresar numeros en precio y cantidad!");
+            JOptionPane.showMessageDialog(null,"Solo puede ingresar numeros en cantidad");
             cantidadP.setText("");
             return;
 
@@ -60,20 +86,29 @@ public class RegistrarProductos extends JFrame {
         try {
             precio = Double.parseDouble(precioP.getText());
         }catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(null,"Solo puede ingresar numeros en precio y cantidad!");
+            JOptionPane.showMessageDialog(null,"Solo puede ingresar numeros en precio");
             precioP.setText("");
             return;
         }
 
-        if(nombre.isEmpty()||decripcion.isEmpty()||cantidadP.getText().isEmpty()||precioP.getText().isEmpty()||subirImagen(true)==null){
-            JOptionPane.showMessageDialog(null, "Todos los campos son obligatorioa");
+        System.out.println(nombre);
+        System.out.println(decripcion);
+        System.out.println(cantidad);
+        System.out.println(precio);
+        System.out.println(imagen);
+
+        if(nombre.isEmpty()||decripcion.isEmpty()||cantidadP.getText().isEmpty()||precioP.getText().isEmpty()||imagen==null){
+            JOptionPane.showMessageDialog(null, "Todos los campos son obligatorios");
         }else {
             try {
-                RegistrarProducto(nombre,decripcion,cantidad,precio,subirImagen(true));
+                RegistrarProducto(nombre,decripcion,cantidad,precio,imagen);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         }
+
+
+
     }
     public  byte[] subirImagen(boolean exitImg ){
         if (!exitImg){
@@ -91,9 +126,6 @@ public class RegistrarProductos extends JFrame {
                     int option = JOptionPane.showConfirmDialog(null, "¿Desea subir esta imagen?", "Confirmar", JOptionPane.YES_NO_OPTION);
                     if (option == JOptionPane.YES_OPTION) {
                         // Obtener direccion de la imagen
-                        // Convertir los bytes a una representación de cadena (por ejemplo, Base64)
-                        String imageDataAsString = Base64.getEncoder().encodeToString(imageData);
-                        rutaimagen.setText(imageDataAsString);
                         JOptionPane.showMessageDialog(null, "La imagen se subió correctamente" );
                         return imageData;
 
@@ -117,7 +149,7 @@ public class RegistrarProductos extends JFrame {
 
         if(conexion != null) {
             try {
-                String sql = "INSERT INTO repuestos (nombre,descripcion, stock, precio, imagen) VALUES (?,?, ?, ?,?)";
+                String sql = "INSERT INTO repuestos (nombreRepuesto,descripcion,stock,precio,imagen) VALUES (?,?, ?, ?,?)";
 
                 try(PreparedStatement stmt = conexion.prepareStatement(sql)){
 
@@ -128,6 +160,11 @@ public class RegistrarProductos extends JFrame {
                     stmt.setBytes(5, imageData);
                     stmt.executeUpdate();
                     JOptionPane.showMessageDialog(null, "Registro Exitoso");
+                    nombreP.setText("");
+                    descripcionP.setText("");
+                    cantidadP.setText("");
+                    precioP.setText("");
+                    imagen=new byte[0];
 
                 }
             }catch (SQLException e){
@@ -141,4 +178,47 @@ public class RegistrarProductos extends JFrame {
             }
         }
     }
+    private void mostrarProductos_tabla() {
+        BaseDatos manejadorBD = new BaseDatos();
+        Connection conexion = manejadorBD.conexionBase();
+        String sql = "SELECT * FROM repuestos";
+
+        try (PreparedStatement statement = conexion.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            // Crear el modelo de tabla con los nombres de las columnas
+            DefaultTableModel tableModel = new DefaultTableModel(new Object[][]{},
+                    new String[]{"Id", "Nombre", "Descripcion","Stock","Precio","Imagen"});
+
+            //Limpiar la tabla
+            for (int i = 0; i < tableR.getRowCount(); i++) {
+                tableModel.removeRow(i);
+                i -= 1;
+            }
+
+            while (resultSet.next()) {
+                int id=resultSet.getInt("IdRepuestos");
+                String nombre = resultSet.getString("nombreRepuesto");
+                String descripcion = resultSet.getString("descripcion");
+                int stock = resultSet.getInt("stock");
+                double precio= resultSet.getDouble("precio");
+                byte[] imagen=resultSet.getBytes("imagen");
+
+                // Agregar fila al modelo de tabla
+                tableModel.addRow(new Object[]{id,nombre,descripcion,stock,precio,imagen});
+            }
+
+            tableR.setModel(tableModel);
+
+            // Aplicar el renderizador personalizado a la columna de imágenes
+            tableR.getColumnModel().getColumn(5).setCellRenderer(new RenderImagen());
+
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Error al obtener información de la base de datos: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+
 }
---
+++ src/RenderImagen.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class RenderImagen extends DefaultTableCellRenderer {
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        if (value instanceof byte[]) {
+            // Si el valor es un array de bytes (imagen)
+            byte[] imageData = (byte[]) value;
+            ImageIcon icon = new ImageIcon(imageData);
+            Image scaledImage = icon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+            icon = new ImageIcon(scaledImage);
+
+            setIcon(icon);
+            setText(null); // Elimina el texto en la celda
+        } else {
+            // Si no es un array de bytes, usar el renderizador predeterminado
+            super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        }
+
+        return this;
+    }
+}
